bc0836f251930e7420544bf70cfb17aa7073ed62
danielSafonyan <daniel.safonyan@gmail.com>
2022-06-13 09:16:56 +0100

    Passed Stage 1

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,1 +1,18 @@
-print('Hello, World!')
+import string
+
+
+def increment_string(my_str):
+    digit_idx = None
+
+    for idx, letter in enumerate(my_str):
+        if letter in string.digits:
+            digit_idx = idx
+            break
+
+    digit_portion = my_str[digit_idx:]
+
+    if int(digit_portion) == 0:
+        return my_str[:-1] + '1'
+
+testing_string = 'hello'
+print(testing_string[:-1])
